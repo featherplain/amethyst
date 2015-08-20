af6ad28814e488b25a71f374dae4fe3c87b5101f
featherplain <info@featherplain.com>
2015-08-20 17:24:07 +0900

    make mobile navigation #5

--- src/js/app/script.js
+++ src/js/app/script.js
@@ -1,22 +1,33 @@
-// magnific popup
-$(function () {
-  $('.modal__btn').magnificPopup({
-    type: 'inline',
-    preloader: false,
-    focus: '#username',
-    modal: true,
+(function($){
+  $(document).ready(function(){
+
+    // *************************************************
+    // Initialize Foundation
+    // *************************************************
+    if(typeof $.prototype.foundation === 'function') {
+      $(document).foundation();
+    }
+    // *************************************************
+    // global menu for mobile
+    //*************************************************
+    var menu = {
+      init: function() {
+        var $toggleBtn   = $('.gNav__btn');
+        var $toggledMenu = $('.gNav__list');
+
+        $toggleBtn.on('click', function(e) {
+          e.preventDefault();
+
+          $toggleBtn.toggleClass('gNav__list--active');
+          $toggledMenu.toggleClass('gNav__list--active');
+
+        });
+      }
+    };
+    menu.init();
   });
-  $(document).on('click', '.modal__btn--dismiss', function (e) {
-    e.preventDefault();
-    $.magnificPopup.close();
-  });
-});
-// add action highlight.js
-hljs.initHighlightingOnLoad();
-// initialization of highlight.js
-$(document).ready(function() {
-  $('pre').each(function(i, block) {
-    hljs.highlightBlock(block);
-  });
-});
-    
+})(jQuery);
+
+
+
+
