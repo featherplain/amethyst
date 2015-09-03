5eb655b431b04d5e44cce66009ce3f6242849f47
featherplain <info@featherplain.com>
2015-09-03 23:01:05 +0900

    modify typography and tweak wp-media #23

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -21,11 +21,15 @@
 				<span><?php echo get_the_author_meta( 'display_name' ); ?></span>
 			</div>
 		</div><!-- .post__meta -->
-
+		<div class="post__image"><img src="http://placehold.jp/1500x590.png" alt=""></div>
 	</header><!-- / .post__header -->
 
 	<div class="post__content">
-		<?php the_content(); ?>
+		<div class="editor-style">
+
+			<?php the_content(); ?>
+
+		</div>
 		<?php
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'othello' ),
@@ -35,8 +39,18 @@
 	</div><!-- .post__content -->
 	<footer class="post__footer">
 	  <div class="post__meta post__meta--footer">
-	    <div class="post__tag"><?php the_tags(); ?></div>
-	    <div class="post__comment"><a href="#">No Comments</a></div>
+
+			<?php the_tags( '<div class="post__tag">' , ',' , '</div>' ); ?>
+
+			<?php // category
+			$cats = get_the_category();
+			$cat_html = '';
+			foreach ( $cats as $cat) {
+				$cat_html = '<div class="post__category"><a href="' . get_category_link( $cat->cat_ID ) . '">' . $cat->name . '</a></div>';
+			}
+			?>
+			<?php echo $cat_html; ?>
+
 	  </div>
 	</footer>
 </article><!-- / #post-## .post -->
