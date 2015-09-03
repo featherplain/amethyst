22fb5ddbd197e2f3e1381602bad83a69f5835e1e
featherplain <info@featherplain.com>
2015-09-04 01:23:47 +0900

    make page && delete content-archive #24

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -9,23 +9,23 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'post--page' ); ?>>
+	<header class="post__header">
+		<?php the_title( '<h1 class="post__title">', '</h1>' ); ?>
+	</header><!-- / .post__header -->
 
-	<div class="entry-content">
-		<?php the_content(); ?>
+	<div class="post__content">
+		<div class="editor-style">
+
+			<?php the_content(); ?>
+
+		</div>
 		<?php
 			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', '_s' ),
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'othello' ),
 				'after'  => '</div>',
 			) );
 		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php edit_post_link( esc_html__( 'Edit', '_s' ), '<span class="edit-link">', '</span>' ); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-## -->
+	</div><!-- .post__content -->
+</article><!-- / #post-## .post -->
 
