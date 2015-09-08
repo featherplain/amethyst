4a1ba622425fcc47b4eb4938828a628d370d31ec
featherplain <info@featherplain.com>
2015-09-09 01:22:55 +0900

    support single column when sidebar is nothing #30

---
+++ template-parts/full-width.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Template Name: Full Width Page
+ *
+ * @since othello
+ */
+
+get_header(); ?>
+
+<div class="l-primary">
+	<?php
+		// Start the Loop.
+		while ( have_posts() ) : the_post();
+
+			// Include the page content template.
+			get_template_part( 'content', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) {
+				comments_template();
+			}
+		endwhile;
+	?>
+</div><!-- / .l-primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
