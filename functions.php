ded43adaf8cf79e0cff53c7593ca40fbf20654b9
featherplain <info@featherplain.com>
2015-09-17 17:23:00 +0900

    remove uneccesary files #57

--- functions.php
+++ functions.php
@@ -220,11 +220,6 @@ add_action( 'wp_enqueue_scripts', 'othello_scripts' );
 require get_template_directory() . '/inc/custom-header.php';
 
 /**
- * Custom template tags for this theme.
- */
-require get_template_directory() . '/inc/template-tags.php';
-
-/**
  * Custom functions that act independently of the theme templates.
  */
 require get_template_directory() . '/inc/extras.php';
