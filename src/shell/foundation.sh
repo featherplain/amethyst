bcbc9b114ac048be6ae1812cd1db342749060260
featherplain <info@featherplain.com>
2016-02-14 15:24:37 +0900

    switch foundation 5 to 6

--- src/shell/foundation.sh
+++ src/shell/foundation.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
 function copy:foundation() {
-  cp -r ./bower_components/foundation/scss/foundation ./src/scss/core/foundation
-  cp ./bower_components/foundation/scss/foundation/_settings.scss ./src/scss/core/_settings.scss
-  cp ./bower_components/foundation/scss/foundation.scss ./src/scss/core/_foundation.scss
+  # scss files
+  mkdir ./src/scss/components/core/foundation
+  cp -r ./node_modules/foundation-sites/scss/components ./src/scss/components/core/foundation
+  cp -r ./node_modules/foundation-sites/scss/forms ./src/scss/components/core/foundation
+  cp -r ./node_modules/foundation-sites/scss/grid ./src/scss/components/core/foundation
+  cp -r ./node_modules/foundation-sites/scss/typography ./src/scss/components/core/foundation
+  cp -r ./node_modules/foundation-sites/scss/util ./src/scss/components/core/foundation
+  cp -r ./node_modules/foundation-sites/scss/_global.scss ./src/scss/components/core/foundation
+  cp ./node_modules/foundation-sites/scss/settings/_settings.scss ./src/scss/components/core/foundation/_settings.scss
+  cp ./node_modules/foundation-sites/scss/foundation.scss ./src/scss/components/core/foundation/_foundation.scss
+  # js files
+  cp -r ./node_modules/foundation-sites/js ./src/js/foundation
   return
 }
 
-if [[ -f ./src/scss/core/_foundation.scss || -f ./src/scss/core/_settings.scss ]]
+if [[ -f ./src/scss/components/core/_foundation.scss || -f ./src/scss/components/core/_settings.scss || -f ./src/js/foundation ]]
 then
   echo 'file already exists/'
 else
