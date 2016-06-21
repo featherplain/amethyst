71c98b93c8f417a479d1c4315d2ee14330762780
featherplain <info@featherplain.com>
2016-06-21 20:48:58 +0900

    fix typo

--- .bin/wp_test.sh
+++ .bin/wp_test.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # Set up WordPress installation.
-export WP_DEVELOP_DIR=/tmp/wordpress/
+export WP_DEVELOP_DIR=/tmp/wordpress
 mkdir -p $WP_DEVELOP_DIR
 # Use the Git mirror of WordPress.
 git clone --depth=1 --branch="$WP_VERSION" git://develop.git.wordpress.org/ $WP_DEVELOP_DIR
 # Set up Amethyst theme information.
-theme_slug=$(amethyst $(pwd))
+theme_slug=$(base $(pwd))
 theme_dir=$WP_DEVELOP_DIR/src/wp-content/themes/$theme_slug
 cd ..
 mv $theme_slug $theme_dir
@@ -16,7 +16,7 @@ echo $WP_DEVELOP_DIR
 cp wp-tests-config-sample.php wp-tests-config.php
 sed -i "s/amethyst_testdb/wordpress_test/" wp-tests-config.php
 sed -i "s/amethyst/root/" wp-tests-config.php
-sed -i "s/randomstring3212//" wp-tests-config.php
+sed -i "s/randummstring3212//" wp-tests-config.php
 # Create WordPress database.
 mysql -e 'CREATE DATABASE wordpress_test;' -uroot
 # Hop into themes directory.
