689fcb1ae59693f95f15aedf16a967e89166a7c4
featherplain <info@featherplain.com>
2016-06-22 21:37:15 +0900

    stack

--- functions.php
+++ functions.php
@@ -112,11 +112,21 @@ add_filter( 'body_class', 'amethyst_body_classes' );
  *
  * @global int $content_width
  */
-function amethyst_content_width() {
+// function amethyst_content_width() {
 	// $GLOBALS['content_width'] = apply_filters( 'amethyst_content_width', 774 );
 	// $content_width = apply_filters( 'amethyst_content_width', 774 );
-	global $content_width;
-	if ( !isset( $content_width ) ) $content_width = 774;
+	// global $content_width;
+	// if ( !isset( $content_width ) ) $content_width = 774;
+// }
+// add_action( 'after_setup_theme', 'amethyst_content_width', 0 );
+function amethyst_content_width() {
+	$amethyst_content_width = 774;
+	/**
+	 * Filter content width
+	 *
+	 * @param integer $amethyst_content_width Default content width.
+	 */
+	$GLOBALS['content_width'] = (int) apply_filters( 'amethyst_content_width', $amethyst_content_width );
 }
 add_action( 'after_setup_theme', 'amethyst_content_width', 0 );
 
