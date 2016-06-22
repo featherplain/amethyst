677477a030b17119cb8290df5de34d367ebb9aa1
featherplain <info@featherplain.com>
2016-06-22 22:31:29 +0900

    fix error

--- footer.php
+++ footer.php
@@ -8,8 +8,6 @@
  * @license GPLv2 or later
  */
 
-if ( ! is_active_sidebar( 'footer-widgets' ) ) :
-
 ?>
 
 		</div><!-- /.l-main -->
@@ -34,14 +32,13 @@ if ( ! is_active_sidebar( 'footer-widgets' ) ) :
 					<div class="siteinfo__social">
 						<nav class="snav">
 							<?php
-							wp_nav_menu( array(
+								wp_nav_menu( array(
 									'theme_location' => 'social_nav',
 									'container'      => false,
 									'items_wrap'     => '<ul id="%1$s" class="snav__list">%3$s</ul>',
 									'link_before'    => '<span class="screen-reader-text">',
 									'link_after'     => '</span>',
-								)
-							);
+								) );
 							?>
 					  </nav>
 					</div>
@@ -54,9 +51,6 @@ if ( ! is_active_sidebar( 'footer-widgets' ) ) :
 
 	</div><!-- /.l-container -->
 
-<?php
-endif;
-?>
 <?php wp_footer(); ?>
 
 </body>
