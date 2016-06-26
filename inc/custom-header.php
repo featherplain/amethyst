47b517139653464cc80eca7ec8a838a72952c175
featherplain <info@featherplain.com>
2016-06-26 09:18:49 +0900

    fix phpmd error

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -60,10 +60,6 @@ function amethyst_header_style() {
 			position: absolute;
 			clip: rect(1px, 1px, 1px, 1px);
 		}
-	<?php
-		// If the user has set a custom color for the text use that.
-		else :
-	?>
 		.site-title a,
 		.site-description {
 			color: #<?php echo esc_attr( $header_text_color ); ?>;
