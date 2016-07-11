cf37ea114f0e2ed42fdd377d31b0123b5a331235
Hisateru Tanaka <tanakahisateru@gmail.com>
2016-07-12 04:15:53 +0900

    Use deploy directive instead of after_success.
    - Build log shows deploy.sh just when really deployed.
    - deploy.sh content is free from Travis env.
    
    Now deploy.sh may be too danger because it can break git repo everywhere.

--- .bin/deploy.sh
+++ .bin/deploy.sh
@@ -2,16 +2,6 @@
 
 set -e
 
-if [[ "false" != "$TRAVIS_PULL_REQUEST" ]]; then
-  echo "Not deploying pull requests."
-  exit
-fi
-
-if [[ "master" != "$TRAVIS_BRANCH" ]]; then
-  echo "Not on the 'master' branch."
-  exit
-fi
-
 rm -rf .git
 rm -r .gitignore
 
